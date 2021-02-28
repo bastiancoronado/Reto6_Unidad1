2e3e8d030cc532fd766550c2bc00b02e9bde9eb0
bastiancoronado <47068225+bastiancoronado@users.noreply.github.com>
2021-02-28 11:42:34 -0500

    Suspenso

--- R6U1_esp32/R6U1_esp32.ino
+++ R6U1_esp32/R6U1_esp32.ino
@@ -1,9 +1,17 @@
-void setup() {
-  // put your setup code here, to run once:
+ void setup() {
+   Serial.begin(9600);
 
-}
+ }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+ void loop() {
+   //vamos a transmitir el 16205
 
-}
+   static uint16_t x = 0xFF;
+
+   if (Serial.available()) {
+     if (Serial.read() == 's') {
+       Serial.write((uint8_t)( x & 0x00FF));
+       Serial.write( (uint8_t)( x >> 8 ));
+     }
+   }
+ }
